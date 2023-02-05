046d3f20abe234fd760577793a99910e7df3caf9
RSkudra <skudru.rinalds@gmail.com>
2023-02-05 20:35:26 +0200

    test1

--- Main_script.ps1
+++ Main_script.ps1
@@ -1,36 +1,20 @@
 # Prompt the user to select from a list of pre-determined Docker containers
-$options = @("pihole", "nginx", "mariadb", "redis")
+$options = @("pihole - DNS server", "nginx - Website host", "mariadb - Database")
 $selectedOption = Read-Host "Select the Docker container you want to install ($($options -join ', '))"
 
 # Prompt the user for the username and IP address of the Debian server
-$username = Read-Host "Enter the username for the Debian server"
-$server = Read-Host "Enter the IP address of the Debian server"
-
-# Connect to the Debian server using SSH
-ssh $username@$server
-
-# Install Docker on the server if it's not already installed
-sudo apt-get update
-sudo apt-get install docker.io -y
-
-# Check if the selected container is in the list of pre-determined options
-if ($options -contains $selectedOption) {
-    switch ($selectedOption) {
-        "pihole" {
-            sudo docker run -d --name pihole -p 53:53/tcp -p 53:53/udp -p 80:80 pihole/pihole
-        }
-        "nginx" {
-            sudo docker run --name nginx -p 80:80 -d nginx
-        }
-        "mariadb" {
-            sudo docker run --name mariadb -e MYSQL_ROOT_PASSWORD=secret -d mariadb
-        }
-        "redis" {
-            sudo docker run --name redis -d redis
-        }
-    }
-    Write-Host "The selected Docker container ($selectedOption) has been installed."
+$isDefault = Read-Host "Is the install changed? (y/n)"
+if ($isDefault -eq "y") {
+    $username = Read-Host "Enter the username for the Debian server"
+    $server = Read-Host "Enter the IP address of the Debian server"
 }
 else {
-    Write-Host "Invalid option selected. Please choose one of the following: ($($options -join ', '))"
+        $username = pi
+        $server = fampi
 }
+
+# Connect to the Debian server and download the Debian script
+ssh $username@$server "wget https://raw.githubusercontent.com/user/repo/master/docker-install.sh"
+
+# Make the Debian script executable and run it
+ssh $username@$server "chmod +x docker-install.sh && ./docker-install.sh"
---
+++ docker-install.sh
@@ -0,0 +1,49 @@
+# Ask the user which Docker containers they want to install
+options=(pihole nginx mariadb redis)
+prompt="Select the Docker containers you want to install (${options[@]}): "
+while read -r -p "$prompt" input; do
+  containers=("$input")
+  break
+done
+
+# Install Docker and Docker Compose
+sudo apt-get update
+sudo apt-get install docker.io docker-compose -y
+mkdir /DockerFiles/Data
+
+# Loop through the selected containers
+for container in "${containers[@]}"; do
+  case $container in
+    pihole)
+      # Download the Docker Compose file for Pi-hole
+      sudo wget https://github.com/RSkudra/Fam-Biz_pi/docker_compose_files/raw/master/docker-compose-pihole.yml
+      mkdir /DockerFiles/Data/PiHole
+      # Start the Pi-hole container using Docker Compose
+      sudo docker-compose up -d
+      ;;
+    nginx)
+      # Download the Docker Compose file for Nginx
+      sudo wget https://github.com/nginxinc/docker-nginx/raw/main/mainline/alpine/docker-compose.yml
+
+      # Start the Nginx container using Docker Compose
+      sudo docker-compose up -d
+      ;;
+    mariadb)
+      # Download the Docker Compose file for MariaDB
+      sudo wget https://github.com/mariadb-corporation/mariadb-docker-container/raw/main/docker-compose.yml
+
+      # Start the MariaDB container using Docker Compose
+      sudo docker-compose up -d
+      ;;
+    redis)
+      # Download the Docker Compose file for Redis
+      sudo wget https://github.com/docker-library/redis/raw/main/docker-compose.yml
+
+      # Start the Redis container using Docker Compose
+      sudo docker-compose up -d
+      ;;
+    *)
+      echo "Invalid option: $container"
+      ;;
+  esac
+done
