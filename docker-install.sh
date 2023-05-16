d0dfc2626eeb791c4122d3cedc46611be00e4c10
RJS <47944724+RJSkudra@users.noreply.github.com>
2023-05-16 23:52:23 +0300

    Update docker-install.sh
    Signed-off-by: RJS <47944724+RJSkudra@users.noreply.github.com>

--- docker-install.sh
+++ docker-install.sh
@@ -4,18 +4,8 @@ sudo apt-get update
 sudo apt-get install docker.io docker-compose -y
 
 sudo usermod -aG docker $USER
-sudo mkdir ./DockerFiles/data/
-
-read -p "Do you need to setup VPN? (y/n)" vpn_install_choice
-if [ $vpn_install_choice = "y" ]; then
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-
-echo "Use the Zerotier One Network ID https://my.zerotier.com/ to connect to the VPN (it's 16 digits long ################)"
-read -p "Enter the ZT ID digits:  " zerotierNetworkID
-
-sudo zerotier-cli join $zerotierNetworkID
-sleep 2
+sudo mkdir ./DockerFiles/ContainerData/
+sudo mkdir ./DockerFile/Downloads
 
 FILE=docker-container-setup.sh
 if test -f "$FILE"; then
